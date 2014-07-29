e6eedb043b80b6e30b90d213f6dbe5341fdcce9b
Devin Brown <devin.g.brown@gmail.com>
2014-07-28 22:00:29 -0600

    non-dry version

--- js/controllers/directions-ctrl.js
+++ js/controllers/directions-ctrl.js
@@ -1,6 +1,7 @@
 angular.module('myApp')
   .controller('DirectionsCtrl', ['$scope', '$modal', '$http', function($scope, $modal, $http){
-    $scope.open = function () {
-      console.log('#1 click');
-    };
+    $scope.open = function () {var modalInstance = $modal.open({
+      templateUrl: './js/templates/modal.html'
+    });
+  };
 }]);
